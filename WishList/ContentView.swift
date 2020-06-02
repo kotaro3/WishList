c0b0c057fb0bf343c011c62246619e8cc4aea221
Satou Koutarou <kohtaro.sato@mobilus.co.jp>
2020-06-02 22:54:19 +0900

    Initial Commit

---
+++ WishList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WishList
+//
+//  Created by Satou Koutarou on 2020/06/02.
+//  Copyright © 2020 Satou Koutarou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
