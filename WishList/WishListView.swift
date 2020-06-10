86186402a9510327770c1dd2f63c985c0c22ce85
Satou Koutarou <kohtaro.sato@mobilus.co.jp>
2020-06-10 13:38:14 +0900

    ファイル整理

--- WishList/WishListView.swift
+++ WishList/WishListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  WishListView.swift
 //  WishList
 //
 //  Created by Satou Koutarou on 2020/06/02.
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct WishListView: View {
     var body: some View {
-        Text("Hello, World!")
+		Text("Hello, World")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        WishListView()
     }
 }
