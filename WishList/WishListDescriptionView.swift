e8dc5324cd7e81648db29c4bd0e7e83b7eed5302
kotaro <kohtaro.sato@mobilus.co.jp>
2020-11-05 11:22:14 +0900

    WishListDescriptionView追加

---
+++ WishList/WishListDescriptionView.swift
@@ -0,0 +1,21 @@
+//
+//  WishListDescriptionView.swift
+//  WishList
+//
+//  Created by kotaro on 2020/11/05.
+//  Copyright © 2020 Satou Koutarou. All rights reserved.
+//
+
+import SwiftUI
+
+struct WishListDescriptionView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct WishListDescriptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        WishListDescriptionView()
+    }
+}
