4df2e99698d5b320c14246a86336c69b29b5dd3d
Satou Koutarou <kohtaro.sato@mobilus.co.jp>
2020-11-24 16:58:06 +0900

    項目編集画面追加

---
+++ WishList/WishListItemEditView.swift
@@ -0,0 +1,21 @@
+//
+//  WishListItemEditView.swift
+//  WishList
+//
+//  Created by Satou Koutarou on 2020/11/24.
+//  Copyright © 2020 Satou Koutarou. All rights reserved.
+//
+
+import SwiftUI
+
+struct WishListItemEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WishListItemEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        WishListItemEditView()
+    }
+}
